97fc1edcd29c5c55e849d746f18af78faab91be4
Aramis jaime <you@example.com>
2024-05-06 22:02:03 -0300

    add redirect after create a new user

--- src/pages/signUp/signUp.tsx
+++ src/pages/signUp/signUp.tsx
@@ -6,16 +6,17 @@ import {
 } from "../../interfaces/interfaces";
 import { Button, Form, H2, Input, PNotes } from "../../style/styles";
 import s from "./signUp.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const {
-    mutate: create,
-    // data,
-    isError,
-    isPending,
-  } = useMutationCustom<UserCredentials, ResCreateUser>({
+  const navigate = useNavigate();
+  const { mutateAsync, isError, isPending, data } = useMutationCustom<
+    UserCredentials,
+    ResCreateUser
+  >({
     url: `${import.meta.env.VITE_SOME_BASE_URL}/user`,
     method: "POST",
+    keys: [],
   });
 
   const checkUserExist = useMutationCustom<
@@ -27,7 +28,7 @@ export default function SignUp() {
   });
 
   const saveUser = async (body: UserCredentials) => {
-    await create({
+    mutateAsync({
       user: body.user,
       password: body.password,
     });
@@ -55,6 +56,10 @@ export default function SignUp() {
     });
   };
 
+  if (data?.status === 200) {
+    navigate("/login");
+  }
+
   return (
     <div className={s.container}>
       <H2>Crear usuario</H2>
@@ -76,7 +81,7 @@ export default function SignUp() {
         />
         <Button type="submit">Crear usuario</Button>
       </Form>
-      {isError ? <PNotes>Error... Intente de nuevo</PNotes> : <></>}
+      {isError ? <PNotes> Error... Intente de nuevo</PNotes> : <></>}
       {isPending ? <PNotes>Cargando...</PNotes> : <></>}
       {!checkUserExist.data?.data.validUser ? (
         <PNotes>Usuario no disponible</PNotes>
